f229ad59582c089aac52a6e9f178994131abe14b
bpoole53 <bpoole53@gmail.com>
2023-07-14 15:09:57 -0500

    submission works, rows formatted, need to fine tune

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,25 +16,29 @@ for (x = 9; x < hours.length; x++) {
                             <div class='days'>
                                 <p id="day${x}${timeOfDay}">&nbsp&nbsp${x}${timeOfDay}</p>
                             </div>
-                            <div class='input'>    
-                                <p id="input${input}">Input Text</p>
-                            </div>
-                            <div class='buttons'>
-                                <button id="button${input}">&#x1F4BE;</p>
-                            </div>
+                            <div class='input'>
+                                <form id = "submit${input}">    
+                                    <input type="text" id="input${input}" placeholder="Enter Event">
+                                </form>    
+                            </div>    
+                            <div class="buttons">
+                                <button form="submit${input}" id="button${input}" type="submit">&#x1F4BE;
+                            </div>                   
                         </div>`);
     }else{    
-    planner.append(`<div class='rows'>
-                        <div class='days'>
-                            <p id="day${x}${timeOfDay}">${x}${timeOfDay}</p>
-                        </div>
-                        <div class='input'>    
-                            <p id="input${input}">Input Text</p>
-                        </div>
-                        <div class='buttons'>
-                            <button id="button${input}" type="submit">&#x1F4BE;</p>
-                        </div>
-                    </div>`);
+        planner.append(`<div class='rows'>
+                            <div class='days'>
+                                <p id="day${x}${timeOfDay}">${x}${timeOfDay}</p>
+                            </div>
+                            <div class='input'>
+                                <form id = "submit${input}">    
+                                    <input type="text" id="input${input}" placeholder="Enter Event">
+                                </form>    
+                            </div>    
+                            <div class="buttons">
+                                <button form="submit${input}" id="button${input}" type="submit">&#x1F4BE;
+                            </div>
+                        </div>`);
                     input++;
      }                
     if (x === 12) {
@@ -43,6 +47,30 @@ for (x = 9; x < hours.length; x++) {
     }
 }
 
+function submitInit() {
+    $('#submit1').submit(function(event) {
+        event.preventDefault();
+        /*localStorage.clear();*/
+        let submission = $('#input1').val();
+
+        console.log(submission);
+
+        saveMessage.textContent = "Event added to local storage &#x2714"
+
+
+  
+    
+        /*$('#table-form tbody').append(newRow);
+ 
+        showInput();*/
+    });
+}
+
+function showInput() {
+    
+}
+
+submitInit();
 console.log(this);
 console.log(hours.length);
 console.log(currentDate);
