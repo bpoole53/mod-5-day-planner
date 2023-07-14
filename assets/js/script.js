120b8b4453147330a71584787faf6987e07971b7
bpoole53 <bpoole53@gmail.com>
2023-07-14 11:11:50 -0500

    rows have 3 elements now, just need to design them properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
 var planner = $('#planner');
 let hours = new Array(13);
 let timeOfDay = "AM";
+var currentDate = document.getElementById('current-date');
+let input = 1;
 
+currentDate.textContent = dayjs()
 //have x be the time of day, i.e. 9am or 1pm or whatever.  Then have timeOfDay switch to "PM" after noon
 for (x = 9; x < hours.length; x++) {
     if (x === 12) {
         timeOfDay = "PM"
     }
-    planner.append(`<p>${x}${timeOfDay}</p>`)
+    planner.append(`<div class='rows'>
+                        <p id="day${x}${timeOfDay}">${x}${timeOfDay}</p>
+                        <p id="input${input}">Input Text</p>
+                        <p id="button${input}">Save Button</p>
+                    </div>`);
+                    input++;
     if (x === 12) {
         x = 0
         hours = new Array(6);
@@ -16,3 +24,5 @@ for (x = 9; x < hours.length; x++) {
 
 console.log(this);
 console.log(hours.length);
+console.log(currentDate);
+console.log(string);
